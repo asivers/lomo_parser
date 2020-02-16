092a9953c4a04bd6b6080da852cec5cc3e30c7c9
asivers <sivers295@gmail.com>
2020-02-16 17:58:23 +0300

    getData and sensibilityAdj methods added

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,31 +1,90 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainClass {
     public static void main(String[] args) {
         try {
-            File calibration = new File("./src/files/calibration.mdrs");
-            BufferedReader reader = new BufferedReader(new FileReader(calibration));
-            double[] calibrationData = new double[326];
-            String line;
-            for (int i = 0; i < 10; i++)
-                line = reader.readLine();
-            for (int i = 0; i < 326; i++) {
-                line = reader.readLine();
-                calibrationData[i] = Double.parseDouble(line.substring(4)) * 100;
+//            sensibilityAdj("1", new double[1]);
+            String calibrationPath = "./src/files/calibration.mdrs";
+            double[] calibrationData = getData(calibrationPath);
+//            for (int i = 0; i < 326; i++) {
+//                System.out.println(calibrationData[i]);
+//            }
+            File data_only = new File("./src/data_only");
+            data_only.mkdir();
+            File calibrated = new File("./src/calibrated");
+            calibrated.mkdir();
+
+            File spectrometer = new File("./src/files/spectrometer");
+            List<File> spectrometerFoldersList = Arrays.asList(spectrometer.listFiles());
+            List<File> spectrometerFilesList;
+            for (File folder : spectrometerFoldersList) {
+                spectrometerFilesList = Arrays.asList(folder.listFiles());
+                for (File file : spectrometerFilesList) {
+                    System.out.println(file);
+                }
             }
-            for (int i = 0; i < 326; i++) {
-                System.out.println(calibrationData[i]);
-            }
-            File dirFrom = new File("./src/files/spectrometer");
-            File[] arrFiles = dirFrom.listFiles();
-            System.out.println(dirFrom);
-            for (int i = 0; i < arrFiles.length; i++)
-                System.out.println(arrFiles[i]);
         }
         catch (Exception e) { }
     }
+
+    public static double[] getData(String path) throws FileNotFoundException, IOException {
+        File calibration = new File(path);
+        BufferedReader reader = new BufferedReader(new FileReader(calibration));
+        double[] data = new double[326];
+        String line;
+        for (int i = 0; i < 10; i++)
+            line = reader.readLine();
+        for (int i = 0; i < 326; i++) {
+            line = reader.readLine();
+            data[i] = Double.parseDouble(line.substring(4));
+        }
+        return data;
+    }
+
+    public static double[] sensibilityAdj(String path, double[] data) {
+        Matcher m = Pattern.compile("\\d+mv").matcher(path);
+        String stringSens;
+        int sens = 0;
+        while (m.find()) {
+            stringSens = m.group();
+            sens = Integer.parseInt(stringSens.substring(0, stringSens.length() - 2));
+        }
+        for (int i = 0; i < data.length; i++) {
+            data[i] = data[i] * sens;
+        }
+        return data;
+    }
+//
+//    public static boolean find(String pattern, String str, boolean write, int lim) {
+//        List<String> allMatches = new ArrayList<>();
+//
+//        while (m.find()) {
+//            allMatches.add(m.group());
+//        }
+//        if (write == true) {
+//            for (int i = 0; i < allMatches.size(); i++) {
+//                String s = "";
+//                for (int j = 3; j < allMatches.get(i).length() - 4; j++) {
+//                    s += allMatches.get(i).charAt(j);
+//                }
+//                allMatches.set(i, s);
+//
+//            }
+//            if (allMatches.size() < lim)
+//                lim = allMatches.size();
+//            for (int i = 0; i < lim; i++) {
+//                System.out.println(allMatches.get(i));
+//            }
+//        }
+//        if (allMatches.size() > 0)
+//            return true;
+//        else
+//            return false;
+//    }
 }
 
 
