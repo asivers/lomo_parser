8d59024217126e4927bd78eac43d0da3da1805fd
asivers <sivers295@gmail.com>
2020-02-16 23:41:05 +0300

    launch from cmd provided

--- src/MainClass.java
+++ src/MainClass.java
@@ -7,35 +7,39 @@ import java.util.regex.Pattern;
 public class MainClass {
     public static void main(String[] args) {
         try {
-            String calibrationPath = "./src/files/calibration.mdrs";
+            String prefix = "/src";
+            if (args.length > 0)
+                prefix = "";
+
+            String calibrationPath = "." + prefix + "/calibration.mdrs";
             double[] calibrationData = removeNoise(getData(calibrationPath));
 
-            new File("./src/not_adjusted").mkdir();
-            new File("./src/sensitivity_adjusted").mkdir();
-            new File("./src/spectrum_adjusted").mkdir();
+            new File("." + prefix + "/not_adjusted").mkdir();
+            new File("." + prefix + "/sensitivity_adjusted").mkdir();
+            new File("." + prefix + "/spectrum_adjusted").mkdir();
 
-            File spectrometer = new File("./src/files/spectrometer");
+            File spectrometer = new File("." + prefix + "/files_spectrometer");
             List<File> spectrometerFoldersList = Arrays.asList(spectrometer.listFiles());
             List<File> spectrometerFilesList;
             String folderName;
             String fileName;
             for (File folder : spectrometerFoldersList) {
                 folderName = folder.getName();
-                new File("./src/not_adjusted/" + folderName).mkdir();
-                new File("./src/sensitivity_adjusted/" + folderName).mkdir();
-                new File("./src/spectrum_adjusted/" + folderName).mkdir();
+                new File("." + prefix + "/not_adjusted/" + folderName).mkdir();
+                new File("." + prefix + "/sensitivity_adjusted/" + folderName).mkdir();
+                new File("." + prefix + "/spectrum_adjusted/" + folderName).mkdir();
                 spectrometerFilesList = Arrays.asList(folder.listFiles());
                 for (File file : spectrometerFilesList) {
                     fileName = file.getName();
 
                     double[] fileData = getData(file.getPath());
-                    writeData("./src/not_adjusted/" + folderName + "/" + fileName, fileData);
+                    writeData("." + prefix + "/not_adjusted/" + folderName + "/" + fileName, fileData);
 
                     double[] sensitivityAdjustedData = sensitivityAdj(fileName, removeNoise(fileData));
-                    writeData("./src/sensitivity_adjusted/" + folderName + "/" + fileName, sensitivityAdjustedData);
+                    writeData("." + prefix + "/sensitivity_adjusted/" + folderName + "/" + fileName, sensitivityAdjustedData);
 
                     double[] spectrumAdjustedData = spectrumAdj(sensitivityAdjustedData, calibrationData);
-                    writeData("./src/spectrum_adjusted/" + folderName + "/" + fileName, spectrumAdjustedData);
+                    writeData("." + prefix + "/spectrum_adjusted/" + folderName + "/" + fileName, spectrumAdjustedData);
                 }
             }
         }
@@ -96,107 +100,4 @@ public class MainClass {
         return data;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//    public static boolean find(String pattern, String str, boolean write, int lim) {
-//        List<String> allMatches = new ArrayList<>();
-//
-//        while (m.find()) {
-//            allMatches.add(m.group());
-//        }
-//        if (write == true) {
-//            for (int i = 0; i < allMatches.size(); i++) {
-//                String s = "";
-//                for (int j = 3; j < allMatches.get(i).length() - 4; j++) {
-//                    s += allMatches.get(i).charAt(j);
-//                }
-//                allMatches.set(i, s);
-//
-//            }
-//            if (allMatches.size() < lim)
-//                lim = allMatches.size();
-//            for (int i = 0; i < lim; i++) {
-//                System.out.println(allMatches.get(i));
-//            }
-//        }
-//        if (allMatches.size() > 0)
-//            return true;
-//        else
-//            return false;
-//    }
 }
-
-
-
-/*
-import java.io.*;
-        import java.util.Arrays;
-        import java.util.List;
-
-public class MainClass {
-    public static void main(String[] args) {
-
-        // stringLength - длина строки, короче которой строки не будут записываться
-
-        // по умолчанию
-        int stringLength = 49;
-        // если задавать из командной строки
-        if (args.length > 0) {
-            stringLength = Integer.parseInt(args[0]);
-        }
-
-        // название папки-источника
-        String from = "polina_files";
-        // название папки куда кладем файлы
-        String to = "polina_new_files";
-
-        // при работе с файлами нужна эта конструкция чтобы ловить исключения
-        try {
-            // папка-источник
-            // "./" значит что находится там же где мы вызываем программу из командной строки (относительный путь)
-            File dirFrom = new File("./" + from + "/");
-            // собираем массив всех файлов из папки источника
-            File[] arrFiles = dirFrom.listFiles();
-            // преобразуем массив в список для удобства работы
-            List<File> lst = Arrays.asList(arrFiles);
-
-            // создаем папку куда кладем файлы
-            File dirTo = new File("./" + to + "/");
-            dirTo.mkdir();
-
-            // для каждого файла из папки-источника
-            for (File f : lst) {
-                // конструкция для чтения файла
-                BufferedReader reader = new BufferedReader(new FileReader(f));
-                // присваиваем адрес новому файлу (извлекаем его имя, и приплюсовываем к нему ./polina_new_files/)
-                String newAddress = "./" + to + "/" + f.toString().substring(f.toString().lastIndexOf('\\') + 1);
-                // собственно создаем новый файл
-                File newFile = new File(newAddress);
-                newFile.createNewFile();
-                // конструкция для записи в файл
-                FileWriter fstream = new FileWriter(newAddress, true);
-                BufferedWriter out = new BufferedWriter(fstream);
-                // объявляем переменную строки
-                String line;
-                // записываем в новый файл те строки из исходного, длина которых больше чем stringLength
-                while ((line = reader.readLine()) != null)
-                    if (line.length() > stringLength)
-                        out.write(line + "\n");
-            }
-        }
-        // при работе с файлами нужна эта конструкция чтобы ловить исключения
-        catch (Exception e) { }
-    }
-}
-*/
